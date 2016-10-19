f2e94c6f9f630e1928def0a752cb2caccf14d845
twoutlook <twcloudwebapp@outlook.com>
2016-10-19 06:36:52 +0000

    äll 5,8,13,19 ready

--- mysite/app001/urls.py
+++ mysite/app001/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 app_name = 'app001'
 urlpatterns = [
-    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^19$', views.index19, name='index19'),
+    url(r'^13$', views.index13, name='index13'),
+    url(r'^8$', views.index08, name='index08'),
     url(r'^me/', views.me, name='me'),
     
     # 範例
--- mysite/app001/views.py
+++ mysite/app001/views.py
@@ -10,9 +10,33 @@ def me(request):
     context = {'current_user':request.user,'page_title':'Me','item_list': []}
     return render(request, 'app001/me.html', context)
 
-# def style19(request):
-#     # if not request.user.is_authenticated:
-#     #      return redirect('/')
-#     item_list = TaichiMove.objects.filter(stylenum=19).order_by('setnum', 'movenum')[:3000]
-#     context = {'current_user':request.user,'page_title':'十九式','item_list': item_list}
-#     return render(request, 'itaiching/style19.html', context)
+def index(request):
+    # if not request.user.is_authenticated:
+    #      return redirect('/')
+    item_list = Mnemonics.objects.filter(num1=19).order_by('num2', 'num2')[:3000]
+    context = {'current_user':request.user,'page_title':'十九式','item_list': item_list}
+    return render(request, 'app001/index.html', context)
+    
+    
+def index19(request):
+    # if not request.user.is_authenticated:
+    #      return redirect('/')
+    item_list = Mnemonics.objects.filter(num1=19).order_by('num2', 'num2')[:3000]
+    context = {'current_user':request.user,'page_title':'19only 十九式','item_list': item_list}
+    return render(request, 'app001/index19.html', context)    
+    
+def index13(request):
+    # if not request.user.is_authenticated:
+    #      return redirect('/')
+    item_list = Mnemonics.objects.filter(num1=13).order_by('num2', 'num2')[:3000]
+    context = {'current_user':request.user,'page_title':'十三式','item_list': item_list}
+    return render(request, 'app001/index13.html', context)   
+    
+def index08(request):
+    # if not request.user.is_authenticated:
+    #      return redirect('/')
+    item_list = Mnemonics.objects.filter(num1=8).order_by('num2', 'num2')[:3000]
+    context = {'current_user':request.user,'page_title':'八式','item_list': item_list}
+    return render(request, 'app001/index13.html', context)   
+    
+    
